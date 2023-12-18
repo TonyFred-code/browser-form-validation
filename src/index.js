cd8493e386e1e3fc6fd6aa8f45eb28d5b6cd4ba0
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-18 17:02:23 +0100

    add logic for restarting form filling from start

--- src/index.js
+++ src/index.js
@@ -44,58 +44,11 @@ function checkZIP() {
     ZIPField.setCustomValidity(constraints[country][1]);
   }
 }
-
-
-const form = document.querySelector("form");
-const email = document.getElementById("mail");
-const error = email.nextElementSibling;
-
-// As per the HTML Specification
-const emailRegExp =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-// Now we can rebuild our validation constraint
-// Because we do not rely on CSS pseudo-class, we have to
-// explicitly set the valid/invalid class on our email field
-window.addEventListener("load", () => {
-  // Here, we test if the field is empty (remember, the field is not required)
-  // If it is not, we check if its content is a well-formed email address.
-  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
-  email.className = isValid ? "valid" : "invalid";
-});
-
-// This defines what happens when the user types in the field
-email.addEventListener("input", () => {
-  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
-  if (isValid) {
-    email.className = "valid";
-    error.textContent = "";
-    error.className = "error";
-  } else {
-    email.className = "invalid";
-  }
-});
-
-// This defines what happens when the user tries to submit the data
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
-  if (!isValid) {
-    email.className = "invalid";
-    error.textContent = "I expect an email, darling!";
-    error.className = "error active";
-  } else {
-    email.className = "valid";
-    error.textContent = "";
-    error.className = "error";
-  }
-});
-
 */
 
 function createCountryOptions() {
   const select = document.querySelector('form select');
+  select.textContent = '';
   let selected = false;
 
   countryList.forEach((country) => {
@@ -261,4 +214,14 @@ form.addEventListener('submit', (e) => {
   //   const successMsgDiv = document.querySelector('.success-msg');
 
   form.classList.add('hide');
+});
+
+const restartForm = document.querySelector('button.restart');
+
+restartForm.addEventListener('click', () => {
+  email.value = '';
+  createCountryOptions();
+  password.value = '';
+  confirmPassword.value = '';
+  form.classList.remove('hide');
 });
